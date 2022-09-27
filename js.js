5ca451a81e4950c2e22958a08f123fa6bc17231c
nikolai <nderguz@yandex.ru>
2022-09-27 15:29:19 +0300

    new_branch1

--- js.js
+++ js.js
@@ -1,7 +1,6 @@
 const button = document.querySelector('button');
 
 button.addEventListener('click', function() {
-	alert('Я же не забыл поменять текст в кнопке?')
+	alert('Я же не забыл поменять текст в кнопке? 🙃')
 })
-
-
+<p class="links__title">QA Engineer</p>
